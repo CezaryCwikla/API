0c636cfbefbc3086d535654e4fd15d5c5d28b3a3
CezaryCwikla <CezaryCwikla@github.com>
2022-06-15 18:13:51 +0000

    zmiany pod ubuntu

--- config.py
+++ config.py
@@ -3,13 +3,14 @@ from pathlib import Path
 from dotenv import load_dotenv
 import urllib.parse
 import cx_Oracle
-password = urllib.parse.quote_plus(os.environ.get('PASSWORD2'))  # '123%40456
-name = urllib.parse.quote_plus(os.environ.get('NAME'))
-ip = urllib.parse.quote_plus(os.environ.get('IP'))
+
 base_dir = Path(__file__).resolve().parent
 env_file = base_dir / '.env'
 load_dotenv(env_file)
-cx_Oracle.init_oracle_client(lib_dir=r"C:\instantclient_21_6")
+password = urllib.parse.quote_plus(os.environ.get('PASSWORD2'))
+name = urllib.parse.quote_plus(os.environ.get('NAME'))
+ip = urllib.parse.quote_plus(os.environ.get('IP'))
+#cx_Oracle.init_oracle_client(lib_dir=r"/home/srvadmin/instantclient_21_6")
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
